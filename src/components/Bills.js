7a4dbb0d9bbaa97477323fe88418566a579c3e13
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-07-22 10:16:47 -0500

    add bill component and style

---
+++ src/components/Bills.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import './Bills.css';
+
+export default function Bills(props) {
+    return (
+        <main className='bills-container'>
+            <header>
+                <h1>Monthly Bills</h1>
+                <h2>$520.00/Month</h2>
+            </header>
+            <section class="monthly-bills-form-container">
+                <form>
+                    <div>
+                        <label for="description">Description</label>
+                        <input type="text" name="description" placeholder="Rent, Utilities, etc." id="description" />
+                    </div>
+                    <div>
+                        <label for="amount">Amount</label>
+                        <input type="number" step="0.01" name="amount" id="amount" min="0.01" />
+                    </div>
+                    <button>Add Bill</button>
+                </form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th colspan="2">Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Rent</td>
+                            <td>$450.00</td>
+                            <td>X</td>
+                        </tr>
+                        <tr>
+                            <td>Electricity</td>
+                            <td>$70.00</td>
+                            <td>X</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <td>Total</td>
+                        <td colspan="2">$520.00</td>
+                    </tfoot>
+                </table>
+            </section>
+        </main>
+    )
+}
