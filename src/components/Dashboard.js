12d47107703b7c3aca1d6dc476f1d06768160136
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-07-22 10:14:22 -0500

    link Category modules

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import CategoryModule from './CategoryModule';
 
 import './Dashboard.css';
 
+
 export default function Dashboard(props) {
+
+    const categories = props.categories.map((category, index) => 
+        <Link to={`category/${category.categoryName}`}>
+         <CategoryModule key={index} {...category} />
+        </Link>
+    )
+
     return (
         <div className='dashboard-container'>
             <header class="main-header">
@@ -11,18 +22,7 @@ export default function Dashboard(props) {
                 </section>
             </header>
             <main>
-                <section class="spending">
-                    <header><h1>Spending</h1></header>
-                    <h2>Remaining $500.00</h2>
-                </section>
-                <section class="debts">
-                    <header><h1>Category [2]</h1></header>
-                    <h2>$2500.00</h2>
-                </section>
-                <section class="savings">
-                    <header><h1>Savings</h1></header>
-                    <h2>$3000.00</h2>
-                </section>
+                {categories}
             </main>
         </div>
     )
