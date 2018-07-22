cf84dbf948eaa7e62b2d3c8ab7c16a36cb4cb633
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-07-22 18:42:53 -0500

    restructure mock data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,36 +5,41 @@ const initialState= {
         username: 'fakeUser'
     },
     monthlySalary: 1800,
-    page: 'signup',
     bills: [
         {
             name: 'Rent',
-            amount: 450
+            amount: 450,
+            user_id: 1
         },
         {
             name: 'Electricity',
-            amount: 80
+            amount: 80,
+            user_id: 1
         },
         {
             name: 'Credit Card',
-            amount: 25
+            amount: 25,
+            user_id: 1
         }
     ],
     categories: [
         {
             id: 1,
             name: 'Spending',
-            amount: 600
+            amount: 600,
+            user_id: 1
         },
         {
             id: 2,
             name: 'Debts',
-            amount: 200
+            amount: 200,
+            user_id: 1
         },
         {
             id: 3,
             name: 'Savings',
-            amount: 200
+            amount: 200,
+            user_id: 1
         }
     ],
     transactions: [
@@ -42,25 +47,29 @@ const initialState= {
             name: 'Groceries',
             date: "07/22/2018",
             amount: 70,
-            category_id: 1
+            category_id: 1,
+            user_id: 1
         },
         {
             name: 'Movie',
             date: '07/21/2018',
             amount: 10,
-            category_id: 1
+            category_id: 1,
+            user_id: 1
         },
         {
             name: 'Credit Card',
             date: '07/20/2018',
             amount: 50,
-            category_id: 2
+            category_id: 2,
+            user_id: 1
         },
         {
             name: 'Birthday Money',
             date: '07/19/2018',
             amount: 100,
-            category_id: 3
+            category_id: 3,
+            user_id: 1
         }
     ]
 };
