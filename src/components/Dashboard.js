58c529737b307cb061ff24536c49d0b4a9cc6ef0
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-07-27 10:22:12 -0500

    add tool tip to pie chart

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,51 +3,105 @@ import {Link} from 'react-router-dom';
 import {PieChart} from 'react-easy-chart';
 import {connect} from 'react-redux';
 
+import ToolTip from './ToolTip';
 import CategoryModule from './CategoryModule';
 
 
 import './Dashboard.css';
 
 
-export function Dashboard(props) {
+export class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const categories = props.categories.map((category, index) => 
-        <Link key={index} to={`category/${category.id}`}>
-         <CategoryModule key={index} {...category} />
-        </Link>
-    )
+        this.state = {
+            showToolTip: false,
+            top: '',
+            left: '',
+            value: '',
+            key: ''
+        }
+    }
 
-    const data = props.categories.map(category => ({
-        key: category.category,
-        value: category.amount
-    }));
+    mouseOverHandler(d, e) {
+        console.log(e);
+        this.setState({
+          showToolTip: true,
+          top: e.y,
+          left: e.x,
+          value: d.value,
+          key: d.data.key
+        });
+      }
+    
+      mouseMoveHandler( d, e) {
+          console.log(d, e);
+        if (this.state.showToolTip) {
+          this.setState({top: e.y, left: e.x});
+        }
+      }
+    
+      mouseOutHandler() {
+        this.setState({showToolTip: false});
+      }
+    
+      createTooltip() {
+        if (this.state.showToolTip) {
+          return (
+            <ToolTip
+              top={this.state.top}
+              left={this.state.left}
+            >
+              {this.state.key} budget: ${this.state.value}/month
+            </ToolTip>
+          );
+        }
+        return false;
+      }
 
-    data.push({
-        key: 'Remaining',
-        value: props.remainingMoney
-    });
+    render() {
 
-    console.log(props.remainingMoney);
+    const categories = this.props.categories.map((category, index) => 
+            <Link key={index} to={`category/${category.id}`}>
+            <CategoryModule key={index} {...category} />
+            </Link>
+        )
 
-    return (
-        <div>
-            <div className='dashboard-container'>
-                <header className="main-header">
-                    <section className="portfolio-data">
-                        <PieChart 
-                            innerHoleSize={200}
-                            labels 
-                            data={data} 
+        const data = this.props.categories.map(category => ({
+            key: category.category,
+            value: category.amount
+        }));
 
-                        />
-                    </section>
-                </header>
-                <main>
-                    {categories}
-                </main>
+        data.push({
+            key: 'Remaining',
+            value: this.props.remainingMoney
+        });
+
+        console.log(this.props.remainingMoney);
+        
+        return (
+            <div>
+                <div className='dashboard-container'>
+                    <header className="main-header">
+                        <section className="portfolio-data">
+                            <PieChart 
+                                innerHoleSize={200}
+                                labels 
+                                data={data} 
+                                mouseOverHandler={this.mouseOverHandler.bind(this)}
+                                mouseOutHandler={this.mouseOutHandler.bind(this)}
+                                mouseMoveHandler={this.mouseMoveHandler.bind(this)}
+                            />
+                            {this.createTooltip()}
+                        </section>
+                    </header>
+                    <main>
+                        {categories}
+                    </main>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 const mapStateToProps = state => ({
