b5e6cc01f304c2eedb24a1aef36eb90f366fae7b
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-07 06:30:50 -0500

    refactor initial state to match database

--- src/reducers/simplify-reducer.js
+++ src/reducers/simplify-reducer.js
@@ -3,83 +3,90 @@ import * as actions from '../actions';
 const uuidv4 = require('uuid/v4');
 
 const initialState = {
-    setupStep: 1,
+    loggedIn: false,
     user: {
-        id: 1,
-        username: 'fakeUser'
-    },
-    monthlySalary: 1800,
-    bills: [{
-            id: 1,
-            bill: 'Rent',
-            amount: 450,
-            user_id: 1
-        },
-        {
-            id: 2,
-            bill: 'Electricity',
-            amount: 80,
-            user_id: 1
-        },
-        {
-            id: 3,
-            bill: 'Credit Card',
-            amount: 25,
-            user_id: 1
-        }
-    ],
-    categories: [{
-            id: 1,
-            category: 'Spending',
-            amount: 600,
-            user_id: 1
-        },
-        {
-            id: 2,
-            category: 'Debts',
-            amount: 200,
-            user_id: 1
-        },
-        {
-            id: 3,
-            category: 'Savings',
-            amount: 200,
-            user_id: 1
-        }
-    ],
-    transactions: [{
-            id: 1,
-            description: 'Groceries',
-            date: "2018-07-22",
-            amount: 70,
-            category_id: 1,
-            user_id: 1
-        },
-        {
-            id: 2,
-            description: 'Movie',
-            date: '2018-07-21',
-            amount: 10,
-            category_id: 1,
-            user_id: 1
-        },
-        {
-            id: 3,
-            description: 'Credit Card',
-            date: '2018-07-20',
-            amount: 50,
-            category_id: 2,
-            user_id: 1
-        },
-        {
-            id: 4,
-            description: 'Birthday Money',
-            date: '2018-07-19',
-            amount: 100,
-            category_id: 3,
-            user_id: 1
-        }
-    ]
+        id: null,
+        username: null,
+        setupStep: 1,
+        monthlySalary: 0,
+        bills: [
+            {
+                id: 1,
+                bill: 'Rent',
+                amount: 450,
+                user_id: 1
+            },
+            {
+                id: 2,
+                bill: 'Electricity',
+                amount: 80,
+                user_id: 1
+            },
+            {
+                id: 3,
+                bill: 'Credit Card',
+                amount: 25,
+                user_id: 1
+            }
+        ],
+        categories: [{
+                id: 1,
+                category: 'Spending',
+                amount: 600,
+                user_id: 1,
+                transactions: [
+                    {
+                        id: 1,
+                        description: 'Groceries',
+                        date: "2018-07-22",
+                        amount: 70,
+                        category_id: 1,
+                        user_id: 1
+                    },
+                    {
+                        id: 2,
+                        description: 'Movie',
+                        date: '2018-07-21',
+                        amount: 10,
+                        category_id: 1,
+                        user_id: 1
+                    },
+                ]
+            },
+            {
+                id: 2,
+                category: 'Debts',
+                amount: 200,
+                user_id: 1,
+                transactions: [
+                    {
+                        id: 3,
+                        description: 'Credit Card',
+                        date: '2018-07-20',
+                        amount: 50,
+                        category_id: 2,
+                        user_id: 1
+                    }
+                ]
+            },
+            {
+                id: 3,
+                category: 'Savings',
+                amount: 200,
+                user_id: 1,
+                transactions: [
+                    {
+                        id: 4,
+                        description: 'Birthday Money',
+                        date: '2018-07-19',
+                        amount: 100,
+                        category_id: 3,
+                        user_id: 1
+                    }
+                ]
+            }
+        ]
+    }
 };
 
 export const simplifyReducer = (state = initialState, action) => {
