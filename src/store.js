006a97f67c1b9da5b50dbb7b369fc8b1d565ea6a
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-06 20:42:56 -0500

    add and connect redux form and reducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import {createStore} from 'redux';
-import {simplifyReducer} from './reducers';
+
+import rootReducer from './reducers';
 import {createBrowserHistory} from 'history';
 import {connectRouter} from 'connected-react-router';
 
 export const history = createBrowserHistory();
 
-export default createStore(connectRouter(history)(simplifyReducer));
+
+export default createStore(connectRouter(history)(rootReducer));
 
