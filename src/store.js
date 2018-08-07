b03b218045babe2b64f7737a2493665dddc57fb2
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-06 22:41:11 -0500

    add redux thunk

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 import {createBrowserHistory} from 'history';
@@ -7,5 +8,5 @@ import {connectRouter} from 'connected-react-router';
 export const history = createBrowserHistory();
 
 
-export default createStore(connectRouter(history)(rootReducer));
+export default createStore(connectRouter(history)(rootReducer), applyMiddleware(thunk));
 
