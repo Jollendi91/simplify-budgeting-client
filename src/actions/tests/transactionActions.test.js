ec1b2212ccfac2a0c8367eda2e0ed3a3adf3bd6b
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-15 13:13:14 -0500

    create update and delete transaction action tests

--- src/actions/tests/transactionActions.test.js
+++ src/actions/tests/transactionActions.test.js
@@ -114,8 +114,179 @@ describe('addTransactionError', () => {
     });
 });
 
-describe('', () => {
-    it('', () => {
+describe('addTransaction', () => {
+    it('Should dispatch addTransactionSuccess', () => {
+        const fakeTransaction = {
+            id: 1,
+            transaction: 'Movie',
+            date: '2018-08-11',
+            amount: 10
+        }
+        const {id,transaction, date, amount} = fakeTransaction;
+        const categoryId = 2;
 
+        global.fetch = jest.fn().mockImplementation(() => {
+            return Promise.resolve({
+                ok: true, 
+                json() {
+                    return fakeTransaction
+                }
+            });
+        });
+
+        const dispatch = jest.fn();
+        const getState = jest.fn().mockImplementation(() => ({
+            auth: {
+                authToken: 1234
+            }
+        }));
+        
+        return actions.addTransaction(transaction, date, amount, categoryId)(dispatch, getState).then(() => {
+            expect(fetch).toHaveBeenCalledWith(`${API_BASE_URL}/transactions/category/${categoryId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${getState().auth.authToken}`
+                },
+                body: JSON.stringify({
+                    transaction,
+                    date,
+                    amount,
+                    category_id: categoryId
+                })
+            });
+            expect(dispatch).toHaveBeenCalledWith(actions.addTransactionSuccess(id, transaction, date, amount, categoryId));
+        });
+    });
+});
+
+// Update Transaction Action
+
+describe('updateTransactionSuccess', () => {
+    it('Should return the action', () => {
+        const {id, transaction, date, amount} = {
+            id: 1, 
+            transaction: "Movie",
+            date: "2018-08-12",
+            amount: 15
+        };
+        const categoryId = 2;
+        const action = actions.updateTransactionSuccess(id, transaction, date, amount, categoryId);
+        expect(action.type).toEqual(actions.UPDATE_TRANSACTION_SUCCESS);
+        expect(action.transactionId).toEqual(id);
+        expect(action.transName).toEqual(transaction);
+        expect(action.transDate).toEqual(date);
+        expect(action.transAmount).toEqual(amount);
+        expect(action.categoryId).toEqual(categoryId);
+    });
+});
+
+describe('updateTransactionError', () => {
+    it('Should return the action', () => {
+        const error = 'error';
+        const action = actions.updateTransactionError(error);
+        expect(action.type).toEqual(actions.UPDATE_TRANSACTION_ERROR);
+        expect(action.error).toEqual(error);
+    });
+});
+
+describe('updateTransaction', () => {
+    it('Should dispatch updateTransactionSuccess', () => {
+        const fakeTransaction = {
+            id: 1,
+            transaction: 'Movie',
+            date: '2018-08-11',
+            amount: 10
+        }
+        const {id,transaction, date, amount} = fakeTransaction;
+        const categoryId = 2;
+
+        global.fetch = jest.fn().mockImplementation(() => {
+            return Promise.resolve({
+                ok: true, 
+                json() {
+                    return
+                }
+            });
+        });
+
+        const dispatch = jest.fn();
+        const getState = jest.fn().mockImplementation(() => ({
+            auth: {
+                authToken: 1234
+            }
+        }));
+        
+        return actions.updateTransaction(id, transaction, date, amount, categoryId)(dispatch, getState).then(() => {
+            expect(fetch).toHaveBeenCalledWith(`${API_BASE_URL}/transactions/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${getState().auth.authToken}`
+                },
+                body: JSON.stringify({
+                    id,
+                    transaction,
+                    date,
+                    amount
+                })
+            });
+            expect(dispatch).toHaveBeenCalledWith(actions.updateTransactionSuccess(id, transaction, date, amount, categoryId));
+        });
+    });
+});
+
+// Delete Transaction Action
+
+describe('deleteTransactionSuccess', () => {
+    it('Should return the action', () => {
+        const transactionId = 1;
+        const categoryId = 2;
+        const action = actions.deleteTransactionSuccess(transactionId, categoryId);
+        expect(action.type).toEqual(actions.DELETE_TRANSACTION_SUCCESS);
+        expect(action.transactionId).toEqual(transactionId);
+        expect(action.categoryId).toEqual(categoryId);
+    });
+});
+
+describe('deleteTransactionError', () => {
+    it('Should return the action', () => {
+        const error = 'error';
+        const action = actions.deleteTransactionError(error);
+        expect(action.type).toEqual(actions.DELETE_TRANSACTION_ERROR);
+        expect(action.error).toEqual(error);
+    });
+});
+
+describe('deleteTransaction', () => {
+    it('Should dispatch deleteTransactionSuccess', () => {
+        const transactionId = 1;
+        const categoryId = 2;
+
+        global.fetch = jest.fn().mockImplementation(() => {
+            return Promise.resolve({
+                ok: true, 
+                json() {
+                    return
+                }
+            });
+        });
+
+        const dispatch = jest.fn();
+        const getState = jest.fn().mockImplementation(() => ({
+            auth: {
+                authToken: 1234
+            }
+        }));
+        
+        return actions.deleteTransaction(transactionId, categoryId)(dispatch, getState).then(() => {
+            expect(fetch).toHaveBeenCalledWith(`${API_BASE_URL}/transactions/${transactionId}`, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${getState().auth.authToken}`
+                }
+            });
+            expect(dispatch).toHaveBeenCalledWith(actions.deleteTransactionSuccess(transactionId, categoryId));
+        });
     });
 });
