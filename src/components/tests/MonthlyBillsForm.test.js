9dfb03df7c13b4ab63b475e83070841e170a7169
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 15:14:15 -0500

    create monthly bill form component tests

--- src/components/tests/MonthlyBillsForm.test.js
+++ src/components/tests/MonthlyBillsForm.test.js
@@ -8,4 +8,22 @@ describe('<MonthlyBillForm />', () => {
         const handleSubmit = jest.fn();
         shallow(<MonthlyBillForm handleSubmit={handleSubmit}/>);
     });
+
+    it('Should run handleSumbit on form submit', () => {
+        const handleSubmit = jest.fn();
+        const wrapper = shallow(<MonthlyBillForm handleSubmit={handleSubmit}/>);
+        wrapper.simulate('submit');
+        expect(handleSubmit).toHaveBeenCalledTimes(1);
+        expect(handleSubmit).toHaveBeenCalledWith(expect.any(Function));
+    });
+
+    it('Should dispatch addBill on onSubmit', () => {
+        const handleSubmit = jest.fn();
+        const dispatch = jest.fn().mockImplementation(() => Promise.resolve());
+        const reset = jest.fn();
+        const wrapper = shallow(<MonthlyBillForm handleSubmit={handleSubmit} dispatch={dispatch} reset={reset}/>);
+        wrapper.instance().onSubmit({bill: 'bill', amount: 100});
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));        
+    });
 });
