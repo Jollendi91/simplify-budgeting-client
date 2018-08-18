010855e1023a76b1f653526ad2327bb5906b8507
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 12:24:25 -0500

    create Category component tests

--- src/components/tests/Category.test.js
+++ src/components/tests/Category.test.js
@@ -2,15 +2,30 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import {Category} from '../Category';
+import TransRow from '../TransRow';
+import NavBar from '../NavBar';
+import FilterForm from '../FilterForm';
+import TransactionForm from '../TransactionForm';
 
 const props = {
     category: {
-        transactions:[]
+        transactions:[{id: 1}]
     }
 }
 
 describe('<Category />', () => {
     it('Renders without crashing', () => {
         shallow(<Category {...props}/>);
+    });
+
+    it('Should dispatch fetchProtectedUser if not loaded', () => {
+        const dispatch = jest.fn();
+        shallow(<Category dispatch={dispatch} notLoaded={true} props={props}/>);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
+    }); 
+
+    it('Should render correct elements', () => {
+        const wrapper = shallow(<Category {...props}/>);
+        expect(wrapper.containsAllMatchingElements([<TransRow />, <NavBar />, <FilterForm />, <TransactionForm />])).toEqual(true);
     });
 });
