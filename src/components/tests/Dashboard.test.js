9e27b12c95450bb8c91f98bfed486cc6d383f210
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 13:50:28 -0500

    create dashboard component tests

--- src/components/tests/Dashboard.test.js
+++ src/components/tests/Dashboard.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import {Dashboard} from '../Dashboard';
+import { CategoryModule } from '../CategoryModule';
 
 const props = {
     categories: [],
@@ -12,5 +13,17 @@ const props = {
 describe('<Dashboard />', () => {
     it('Renders without crashing', () => {
         shallow(<Dashboard {...props}/>);
+    });
+
+    it('Should dispatch fetchProtectedUser if not loaded', () => {
+        const dispatch = jest.fn();
+        shallow(<Dashboard {...props} notLoaded={true} dispatch={dispatch}/>);
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
+    });
+
+    it('Should render the correct elements', () => {
+        const wrapper = shallow(<Dashboard {...props}/>);
+        expect(wrapper.containsMatchingElement(<CategoryModule />));
     });
 });
