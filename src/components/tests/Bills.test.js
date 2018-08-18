a388812412b7306e5827b37e3516c318d8966aff
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 12:35:58 -0500

    create Bills component tests

--- src/components/tests/Bills.test.js
+++ src/components/tests/Bills.test.js
@@ -2,14 +2,28 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import {Bills} from '../Bills';
+import { BillRow } from '../BillRow';
+import { NavBar } from '../NavBar';
+import { MonthlyBillForm } from '../MonthlyBillsForm';
 
 const props = {
-    bills: [],
+    bills: [{id: 1}],
     billsTotal: 0
 }
 
 describe('<Bills />', () => {
     it('Renders without crashing', () => {
         shallow(<Bills {...props}/>);
+    });
+
+    it('Should dispatch fetchProtectedUser if not loaded', () => {
+        const dispatch = jest.fn();
+        shallow(<Bills dispatch={dispatch} notLoaded={true} {...props}/>);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
+    });
+
+    it('Should render correct elements', () => {
+        const wrapper = shallow(<Bills {...props}/>);
+        expect(wrapper.containsAllMatchingElements([<BillRow />,<NavBar />, <MonthlyBillForm />, ]));
     });
 });
