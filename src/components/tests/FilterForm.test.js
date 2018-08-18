9703c5b6d5131893a57aea1a97a2cfbfbe4d10c7
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 14:06:28 -0500

    create filter form component tests

--- src/components/tests/FilterForm.test.js
+++ src/components/tests/FilterForm.test.js
@@ -8,4 +8,24 @@ describe('<FilterForm />', () => {
         const handleSubmit =jest.fn();
         shallow(<FilterForm handleSubmit={handleSubmit}/>);
     });
+
+    it('Should run handleSubmit on form submit', () => {
+        const handleSubmit = jest.fn();
+        const wrapper = shallow(<FilterForm handleSubmit={handleSubmit} />);
+        wrapper.simulate('submit');
+        expect(handleSubmit).toHaveBeenCalledTimes(1);
+        expect(handleSubmit).toHaveBeenCalledWith(expect.any(Function));
+    });
+
+    it('Should run updateFilters and dispatch fetchTransactions on onSumbit', () => {
+        const dispatch = jest.fn();
+        const handleSubmit = jest.fn();
+        const updateFilters = jest.fn();
+        const wrapper = shallow(<FilterForm dispatch={dispatch} handleSubmit={handleSubmit} updateFilters={updateFilters}/>);
+        wrapper.instance().onSubmit({filterMonth: 7, filterYear: 2018});
+        expect(updateFilters).toHaveBeenCalledTimes(1);
+        expect(updateFilters).toHaveBeenCalledWith(7, 2018);
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
+    });
 });
