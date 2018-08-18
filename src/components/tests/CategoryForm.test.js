ce3898a3912f2a4b2c91fe663cffb82c7a379c1f
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 13:08:52 -0500

    create category form component tests

--- src/components/tests/CategoryForm.test.js
+++ src/components/tests/CategoryForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import {CategoryForm} from '../CategoryForm';
 
@@ -7,5 +7,23 @@ describe('<CategoryForm />', () => {
     it('Renders without crashing', () => {
         const handleSubmit = jest.fn();
         shallow(<CategoryForm handleSubmit={handleSubmit}/>);
+    });
+
+    it('Should dispatch handleSubmit on form submit', () => {
+        const handleSubmit = jest.fn();
+        const wrapper = shallow(<CategoryForm handleSubmit={handleSubmit} />);
+        wrapper.simulate('submit');
+        expect(handleSubmit).toHaveBeenCalledTimes(1);
+        expect(handleSubmit).toHaveBeenCalledWith(expect.any(Function));
+    });
+
+    it('Should dispatch addCategory on onSubmit', () => {
+        const dispatch = jest.fn().mockImplementation(() => Promise.resolve());
+        const handleSubmit = jest.fn();
+        const reset = jest.fn();
+        const wrapper = shallow(<CategoryForm handleSubmit={handleSubmit} dispatch={dispatch} reset={reset}/>);
+        wrapper.instance().onSubmit({category: 'category', amount: 10});
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
     });
 });
