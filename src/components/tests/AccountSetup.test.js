6ab9e6e1f98024dd6463e01bec3a55506e9bc1ab
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-17 23:15:09 -0500

    test account setup mount dispatch

--- src/components/tests/AccountSetup.test.js
+++ src/components/tests/AccountSetup.test.js
@@ -2,9 +2,17 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import {AccountSetup} from '../AccountSetup';
+import {fetchProtectedUser} from '../../actions/protected-data';
 
 describe('<AccountSetup />', () => {
     it('Renders without crashing', () => {
         shallow(<AccountSetup />);
+    });
+
+    it('Should dispatch fetchProtectedUser if no user info in store', () => {
+        const dispatch = jest.fn().mockImplementation(() => Promise.resolve({}));
+        mount(<AccountSetup notLoaded={true} dispatch={dispatch}/>);
+        const callback = fetchProtectedUser();
+        expect(dispatch).toHaveBeenCalledWith(callback);
     });
 });
