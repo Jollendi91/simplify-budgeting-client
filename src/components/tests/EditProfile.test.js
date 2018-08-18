c48e350028406e4e860dd92c663d53de8116264a
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-08-18 13:54:36 -0500

    create edit profile component tests

--- src/components/tests/EditProfile.test.js
+++ src/components/tests/EditProfile.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import {EditProfile} from '../EditProfile';
+import { MonthlyPayEdit } from '../MonthlyPayEdit';
+import { MonthlyBillsSetup } from '../MonthlyBillsSetup';
+import { CategorySetup } from '../CategorySetup';
 
 describe('<EditProfile />', () => {
     it('Renders without crashing', () => {
         shallow(<EditProfile />);
+    });
+
+    it('Should render the correct elements', () => {
+        const wrapper = shallow(<EditProfile />);
+        expect(wrapper.containsAllMatchingElements([<MonthlyPayEdit />, <MonthlyBillsSetup />, <CategorySetup/>]));
+    });
+
+    it('Should dispatch fetchProtectedUser if not loaded', () => {
+        const dispatch = jest.fn();
+        shallow(<EditProfile dispatch={dispatch} notLoaded={true} />);
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenCalledWith(expect.any(Function));
     });
 });
